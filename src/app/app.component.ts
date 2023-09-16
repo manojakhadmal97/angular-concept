7bd07d5b4d597bb271fdd16eda556fde2e39ed66
manojakhadmal97 <manojakhadmal97@gmail.com>
2023-09-16 15:11:06 +0530

    viewChild

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, AfterViewInit, AfterViewChecked } from '@angular/core';
+import { ChildComponent } from './child/child.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
   title = 'concept';
+  dataFromChild : string = '';
+
+  @ViewChild(ChildComponent) childComp !: ChildComponent;
+
+  constructor() {
+
+  }
+
+  ngAfterViewInit(): void {
+    console.log('Child Data : ', this.childComp.childData);
+  }
+
 }
---
+++ src/app/child/child.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-child',
+  templateUrl: './child.component.html',
+  styleUrls: ['./child.component.css']
+})
+export class ChildComponent {
+
+  childData : string = 'This is Child Data';
+
+}
